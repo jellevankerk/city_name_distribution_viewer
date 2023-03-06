0bc580b6a9342e5283113dc98d5040026d5d6936
jellevankerk <jelle.95@live.nl>
2023-03-06 22:48:11 +0100

    write doc strings

--- src/extraction.py
+++ src/extraction.py
@@ -1,39 +1,84 @@
 import sys
 import pandas as pd
+from typing import List, Dict
 from pprint import pprint
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 class WikiDataQueryResults:
-    def __init__(self, query):
+    """
+    A class that can be used to query data from Wikidata using SPARQL and return the results as a Pandas DataFrame or a list
+    of values for a specific key.
+    """
+    def __init__(self, query: str):
+        """
+        Initializes the WikiDataQueryResults object with a SPARQL query string.
+
+        :param query: A SPARQL query string.
+        """
         self.user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
         self.endpoint_url = "https://query.wikidata.org/sparql"
         self.sparql = SPARQLWrapper(self.endpoint_url, agent=self.user_agent)
         self.sparql.setQuery(query)
         self.sparql.setReturnFormat(JSON)
-    
-    @staticmethod
-    def __transform2dicts(results):
+
+    def __transform2dicts(self, results: List[Dict]) -> List[Dict]:
+        """
+        Helper function to transform SPARQL query results into a list of dictionaries.
+
+        :param results: A list of query results returned by SPARQLWrapper.
+        :return: A list of dictionaries, where each dictionary represents a result row and has keys corresponding to the
+        variables in the SPARQL SELECT clause.
+        """
         new_results = []
         for result in results:
             new_result = {}
             for key in result:
                 new_result[key] = result[key]['value']
             new_results.append(new_result)
-                
         return new_results
-    
-    def _load(self):
-        results = self.sparql.query().convert()['results']['bindings']
-        results = self.__transform2dicts(results)
-        return results    
-    
-    def load_as_dataframe(self):
+
+    def load_as_dataframe(self) -> pd.DataFrame:
+        """
+        Executes the SPARQL query and returns the results as a Pandas DataFrame.
+
+        :return: A Pandas DataFrame representing the query results.
+        """
         results = self._load()
         return pd.DataFrame.from_dict(results)
-    
-    def load_as_list(self, key):
+
+    def load_as_list(self, key: str) -> List[str]:
+        """
+        Executes the SPARQL query and returns a list of values for a specific key.
+
+        :param key: The key for which to retrieve values.
+        :return: A list of string values for the specified key.
+        """
         results = self._load()
         return [x[key] for x in results]
-    
-    
 
+    def _load(self) -> List[Dict]:
+        """
+        Helper function that loads the data from Wikidata using the SPARQLWrapper library, and transforms the results into
+        a list of dictionaries.
+
+        :return: A list of dictionaries, where each dictionary represents a result row and has keys corresponding to the
+        variables in the SPARQL SELECT clause.
+        """
+        results = self.sparql.query().convert()['results']['bindings']
+        results = self.__transform2dicts(results)
+        return results
+
+
+if __name__ == "__main__":
+    query = """
+    SELECT ?country ?iso_code
+    WHERE {
+    ?country wdt:P31 wd:Q6256 .
+    ?country wdt:P297 ?iso_code .
+    }
+    ORDER BY ?country
+    """
+
+    wd_results = WikiDataQueryResults(query)
+    iso_codes = wd_results.load_as_list('iso_code')
+    pprint(iso_codes)
